8a5b907e079dd9cd0f7ddb3ca1857ca85aeed150
ssssooooso <seo.ahyeong0608@gmail.com>
2024-07-02 13:59:08 +0900

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!2";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
